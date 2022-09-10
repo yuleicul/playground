eb712dfb0315d4109b8e6ab42d67d05e9475668b
Yulei Chen <yulei_chen@epam.com>
2022-09-10 14:11:18 +0800

    react-native: add navigation

---
+++ react-native/AwesomeProject/screens/FormScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { SafeAreaView, View, Text } from 'react-native'
+
+const FormScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text>Hello Screen</Text>
+    </SafeAreaView>
+  )
+}
+
+export default FormScreen
