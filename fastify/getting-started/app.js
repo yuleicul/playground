e7a9d1821fb202cbc44319b7e753b407c0b7d278
Yulei Chen <yulei_chen@epam.com>
2024-03-05 10:27:48 +0800

    fastify: getting-started

---
+++ fastify/getting-started/app.js
@@ -0,0 +1,17 @@
+import Fastify from "fastify"
+
+const fastify = Fastify({
+  logger: true,
+})
+
+fastify.get("/", (request, reply) => {
+  reply.send({ hello: "world" })
+})
+
+fastify.listen({ port: 3000 }, (err, address) => {
+  if (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+  // Server is now listening on ${address}
+})
